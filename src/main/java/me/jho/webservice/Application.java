0bc447613fcfbf7834ece3f8ba4a8cb73102f888
Joo-Ho Lee <jooho_lee@outlook.kr>
2019-02-04 20:36:49 +0900

    CLN: me.jho.springwebservice -> me.jho.webservice

--- src/main/java/me/jho/webservice/Application.java
+++ src/main/java/me/jho/webservice/Application.java
@@ -1,13 +1,13 @@
-package me.jho.springwebservice;
+package me.jho.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebserviceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWebserviceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
