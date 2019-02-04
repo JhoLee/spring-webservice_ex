94c15a698456e3e2c03f6282ea27f21419fc9b48
Joo-Ho Lee <jooho_lee@outlook.kr>
2019-02-04 22:31:00 +0900

    ENH: 첫 도메인 코드 생성

--- src/main/java/me/jho/webservice/Application.java
+++ src/main/java/me/jho/webservice/Application.java
@@ -1,5 +1,6 @@
 package me.jho.webservice;
 
+import me.jho.webservice.domain.posts.Posts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
